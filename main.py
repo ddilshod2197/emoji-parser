6cff9b82fe04068fcb9fcf71ccd083245155186e
Dilshod <ddilshod2197@gmail.com>
2026-04-20 23:59:48 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,45 @@
+import re
+
+class EmojiParser:
+    def __init__(self):
+        self.emoji_pattern = re.compile(r'[:][a-zA-Z0-9_]+[:]')
+
+    def parse(self, text):
+        return self.emoji_pattern.sub(r'\\\1', text)
+
+# Misol:
+parser = EmojiParser()
+text = "Men :smile: uchun :love: qiziqaman"
+print(parser.parse(text))
+```
+
+```python
+import re
+
+class EmojiParser:
+    def __init__(self):
+        self.emoji_pattern = re.compile(r'[:][a-zA-Z0-9_]+[:]')
+
+    def parse(self, text):
+        return self.emoji_pattern.sub(r'\\\1', text)
+
+# Misol:
+parser = EmojiParser()
+text = "Men :smile: uchun :love: qiziqaman"
+print(parser.parse(text))
+```
+
+```python
+import re
+
+class EmojiParser:
+    def __init__(self):
+        self.emoji_pattern = re.compile(r'[:][a-zA-Z0-9_]+[:]')
+
+    def parse(self, text):
+        return self.emoji_pattern.sub(r'\\\1', text)
+
+# Misol:
+parser = EmojiParser()
+text = "Men :smile: uchun :love: qiziqaman"
+print(parser.parse(text))
